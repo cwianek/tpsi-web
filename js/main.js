f6d6e2b94aaccfd8e0073aa34c2810aaa3b2b4e3
adam cwian <cwianek@gmail.com>
2018-06-07 23:56:27 +0200

    fetch data from server

--- js/main.js
+++ js/main.js
@@ -1,43 +1,102 @@
 "use strict";
 
-var DataManager = function(){
-    this.data = ko.observableArray();
-    this.baseUrl = 'http://localhost:9998/';
+const baseUrl = 'http://localhost:9998/';
+
+var DataManager = function () {
+    var self = this;
 };
 
-DataManager.prototype.fetchStudents = function () {
-    var mapping={
-        create: function (options) {
-            return
-        }
-    };
-
+DataManager.prototype.fetch = function (name) {
     return $.ajax({
-        url: this.baseUrl + "students",
+        url: baseUrl + name,
         type: "GET",
-        dataType : "json",
+        dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function (data) {
-            console.log(data);
-            var mapping = ko.mapping.fromJSON(data);
-            return mapping;
+            return data;
         }
     })
 };
 
+DataManager.prototype.add = function (name, data) {
+    return $.ajax({
+        url: baseUrl + name,
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: data,
+        success: function (data) {
+            return data;
+        }
+    })
+};
+
+DataManager.prototype.delete = function (name, index) {
+    return $.ajax({
+        url: baseUrl + name + '/' + index,
+        type: "DELETE",
+        dataType: "json",
+        success: function (data) {
+            return data;
+        }
+    })
+};
+
+DataManager.prototype.fetchStudents = function () {
+    return this.fetch("students");
+};
+
+DataManager.prototype.fetchCourses = function () {
+    return this.fetch("subjects");
+};
+
 function AppViewModel() {
     var self = this;
+    self.students = ko.mapping.fromJS([]);
+    self.courses = ko.mapping.fromJS([]);
 
-    var dataManager = new DataManager();
-    var students = dataManager.fetchStudents().then(function (data) {
-        self.students = data;
-        ko.applyBindings(self);
-    });
+    self.addStudent = function () {
+        self.students.push({index: null, name: "", surname: "", birthdate: ""})
+    };
+
+    self.editStudent = function (data) {
+        dataManager.add("students", JSON.stringify({
+            name: data.name(),
+            surname: data.surname(),
+            birthdate: data.birthdate()
+        }));
+    };
+
+    self.deleteStudent = function (data) {
+        dataManager.delete("students", data.index())
+    };
+
+    self.addCourse = function () {
+        self.courses.push({name: "", lecturer: ""})
+    };
+
+    self.editCourse = function (data) {
+        console.log(data);
+        dataManager.add("subjects", JSON.stringify({
+            name: data.name(),
+            lecturer: data.lecturer()
+        }));
+    };
+
+    self.deleteCourse = function (data) {
+        dataManager.delete("subjects",data.id())
+    };
 }
 
 var appViewModel = new AppViewModel();
+var dataManager = new DataManager();
+dataManager.fetchStudents().then(function (data) {
+    ko.mapping.fromJS(data, appViewModel.students);
+});
+dataManager.fetchCourses().then(function (data) {
+    ko.mapping.fromJS(data, appViewModel.courses);
+});
 
 $(function () {
     ko.applyBindings(appViewModel);
 });
-
